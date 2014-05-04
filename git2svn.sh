9011b2c672c9d62157574d0026ff86143f8d9741
Shaofeng Niu <shaofengniu@gmail.com>
2014-05-04 14:05:01 +0800

    add git2svn script

---
+++ git2svn.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+
+if [[ "${CI_BUILD_REF_NAME}" != "master" ]]; then
+    exit 0
+fi
+
+username=""
+password=""
+include_files=""
+
+svn_repo="$1"
+if [[ -z "$svn_repo" ]]; then
+    err "Please specify svn repo url"
+    exit 1
+fi
+
+shift
+
+while getopts 'u:p:i:' flag; do
+    case "${flag}" in
+        u) username="${OPTARG}" ;;
+        p) password="${OPTARG}" ;;
+        i) include_files="${include_files} ${OPTARG}" ;;
+        *) err "Unexpected option ${flag}"; exit 1 ;;
+        
+    esac
+done
+
+if [[ -z "${username}" ]]; then
+    err "Please specify svn username"
+    exit 1
+fi
+
+if [[ -z "${password}" ]]; then
+    err "Please specify svn password"
+    exit 1
+fi
+
+
+if [ -d .git/svn ]; then
+    rm -rf .git/svn
+fi
+
+commit_message="$(git log --oneline HEAD -n 1)"
+
+svn co --username "${username}" --password "${password}" "${svn_repo}" .git/svn
+
+git ls-files | xargs -I{} rsync -R {} .git/svn
+
+for file in "${include_files}"; do
+    rsync -R $file .git/svn
+done
+
+cd .git/svn
+
+for file in "${include_files}"; do
+    svn add --force $file
+done
+
+echo "${commit_message}" > commit_message
